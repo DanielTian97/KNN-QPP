89fc6f6840a3aa9ea50c9a071b2b00e204642d96
Fangzheng Tian <ftian@AD.DCS.GLA.AC.UK>
2024-04-16 20:33:12 +0100

    to get the results for lambda=1

--- src/main/java/experiments/TRECDLQPPEvaluator.java
+++ src/main/java/experiments/TRECDLQPPEvaluator.java
@@ -131,7 +131,7 @@ public class TRECDLQPPEvaluator {
         OptimalHyperParams p = new OptimalHyperParams();
 
         for (int numVariants=1; numVariants<=maxNumVariants; numVariants++) {
-            for (float l = 0; l <= 1; l += Constants.QPP_COREL_LAMBDA_STEPS) {
+            for (float l = 1; l <= 1.1; l += Constants.QPP_COREL_LAMBDA_STEPS) {
                 TauAndSARE tauAndSARE = runExperiment(baseModelName,
                         searcher, knnRelModel, evaluatorTrain,
                         trainQueries, topDocsMap, l, numVariants, targetMetric);
