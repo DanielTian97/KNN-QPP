95e88675d192ab492bb970fffe8c3ba4d2cea7f0
Fangzheng Tian <ftian@AD.DCS.GLA.AC.UK>
2024-04-26 14:09:04 +0100

    index BEIR - add constants

--- src/main/java/retrieval/Constants.java
+++ src/main/java/retrieval/Constants.java
@@ -62,4 +62,11 @@ public interface Constants {
     String QPP_JM_VARIANTS_FILE_SBERT = "variants/trecdl_qv_sbert.csv";
     String QPP_JM_VARIANTS_QID_FILE_SBERT = "variants/trecdl_qv_sbert_qid.csv";
     String QPP_JM_SCORE_FILE_SBERT = "variants/trecdl_qv_sbert_score.csv";
+
+    String COVID_COLL = "data-BEIR/collection.tsv"; 
+    String COVID_INDEX =  "beir-index/COVID/";
+    String FEVER_COLL = "data-BEIR/collection.tsv";
+    String FEVER_INDEX = "beir-index/FEVER/" 
+    String WEBIS_COLL = "data-BEIR/collection.tsv";
+    String WEBIS_INDEX = "beir-index/WEBIS/";
 }
