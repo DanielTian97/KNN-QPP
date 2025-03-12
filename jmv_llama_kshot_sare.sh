0fdae4f4648d140d883f018fce9e86073f50d7fe
Fangzheng Tian <ftian@AD.DCS.GLA.AC.UK>
2025-03-12 21:15:35 +0000

    debug

--- jmv_llama_kshot_sare.sh
+++ jmv_llama_kshot_sare.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ $# -lt 8 ]
+if [ $# -lt 9 ]
 then
-        echo "Usage: $0 <dl19 res> <dl20 res> <ap/ndcg> <nqc/uef> <sbert/bm25/sbertr/bm25r--(qv_method)> <extend_1?> <useRBO?> <k> <lambda>"
+        echo "Usage: $0 <dl19 res> <dl20 res> <ap/ndcg> <nqc/uef> <sbert/bm25/sbertr/bm25r--(qv_method)> <extend_1?> <useRBO?> <p> <k> <lambda>"
         exit
 fi
 
-mvn exec:java -Dexec.mainClass="experiments.TRECDLQPPEvaluatorWithGenVariantsKShotLlamaSARE" -Dexec.args="$1 $2 $3 $4 $5 $6 $7 $8 $9"
+mvn exec:java -Dexec.mainClass="experiments.TRECDLQPPEvaluatorWithGenVariantsKShotLlamaSARE" -Dexec.args="$1 $2 $3 $4 $5 $6 $7 $8 $9 $10"
